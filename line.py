0b66f3d9cfcd5318ddb45ad932257038fb91639c
mora-cier <mcier@mail.austral.edu.ar>
2025-04-14 16:04:16 -0300

    Mora line.py

--- line.py
+++ line.py
@@ -6,9 +6,9 @@ X1 = float(input("Ingrese el coheficiente X1:"))
 X2 = float(input("Ingrese el coheficiente X2:"))
 
 print(f"El coeficiente A de su ecuacion de la recta es: {A}")
-print(f"El coeficiente A de su ecuacion de la recta es: {B}")
-print(f"El coeficiente A de su ecuacion de la recta es: {X1}")
-print(f"El coeficiente A de su ecuacion de la recta es: {X2}")
+print(f"El coeficiente B de su ecuacion de la recta es: {B}")
+print(f"El coeficiente X1 de su ecuacion de la recta es: {X1}")
+print(f"El coeficiente X2 de su ecuacion de la recta es: {X2}")
 
 print("\nPara la siguiente ecuación:")
 print(f"\tY = {A}X + {B}\n")
