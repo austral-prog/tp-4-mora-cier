36365f9338c2dc407bede38a479ee5f47e39817f
mora-cier <mcier@mail.austral.edu.ar>
2025-04-14 15:36:17 -0300

    Mora leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,8 @@
 def leap_year():
-    print("TO DO")
+year = int(input("Ingrese un año: "))
+if (year%4) == 0:
+print(f"El año {year} es bisiesto")
+elif (year%400) == 0:
+print(f"El año {year} es bisiesto")
+else:
+print(f"El año {year} no es bisiesto")
